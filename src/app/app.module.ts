234e4ca426b712746d5f5a6b8fa1fd954d47681a
AndrejKlukaa <andrej.kljuka@gmail.com>
2023-11-10 13:03:46 +0200

    added prettier and set on push by default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeheComponent } from './hehe/hehe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeheComponent
   ],
   imports: [
     BrowserModule,
